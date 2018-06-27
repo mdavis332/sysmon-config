ddbba65b3dcbf13368a50ca912bb9b7d21fe4994
Michael Davis <mdavis332@gmail.com>
2018-06-27 10:03:07 -0500

    Fixed TLS download issue
    added SecurityProtocolType::Tls12 to fix download issue.

--- Graylog_Content_Pack/Install_Sidecar.bat
+++ Graylog_Content_Pack/Install_Sidecar.bat
@@ -3,7 +3,7 @@ cd %temp%
 set /p glg= "[+] What's the Graylog Server name or IP?  "
 echo [+] Server set to: %glg%
 echo [+] Downloading Graylog Sidecar to: %temp%\Sidecar.exe...
-@powershell (new-object System.Net.WebClient).DownloadFile('https://github.com/Graylog2/collector-sidecar/releases/download/0.1.6/collector_sidecar_installer_0.1.6-1.exe','%temp%\Sidecar.exe')"
+@powershell "[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12;(new-object System.Net.WebClient).DownloadFile('https://github.com/Graylog2/collector-sidecar/releases/download/0.1.6/collector_sidecar_installer_0.1.6-1.exe','%temp%\Sidecar.exe')"
 start /wait Sidecar.exe /S -SERVERURL=https://%glg%:443/api -TAGS="windows"
 echo [+] Executing Script to edit content of sidecar configuration...
 REM @powershell -nop -c "iex(New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/mdavis332/sysmon-config/master/Graylog_Content_Pack/sidecar.ps1')"
