a9c94dc9972af9194a62d066724960bf81d6fc1e
Michael Davis <mdavis332@gmail.com>
2018-06-27 10:13:18 -0500

    Fix TLS issues
    Add SecurityProtocolType::Tls12 to fix download issues to Github. Also fix missing double quote.

--- Auto_Update.bat
+++ Auto_Update.bat
@@ -1,5 +1,5 @@
 @echo on
 cd C:\ProgramData\sysmon\
-@powershell (new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/mdavis332/sysmon-config/master/sysmonconfig-export.xml','C:\ProgramData\sysmon\sysmonconfig-export.xml')"
+@powershell "[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12;(new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/mdavis332/sysmon-config/master/sysmonconfig-export.xml','C:\ProgramData\sysmon\sysmonconfig-export.xml')"
 sysmon64 -c sysmonconfig-export.xml
 exit
